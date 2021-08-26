03ad4f44340433dec1f26b2caa4c8a2036bb8064
Woodrow Crawford <woodrowcrawford03@gmail.com>
2021-08-26 12:15:30 -0500

    Created a box that changes color when it is awake or asleep

---
+++ Assets/Scripts/WakeUpMaterialBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class WakeUpMaterialBehavior : MonoBehaviour
+{
+    public Material awakeMaterial = null;
+    public Material asleepMaterial = null;
+
+    private Rigidbody _rigidbody = null;
+    private MeshRenderer _renderer = null;
+
+    private bool _wasAsleep = false;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _renderer = GetComponent<MeshRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Set material to asleep if rigidbody is asleep
+        if (_wasAsleep && _rigidbody.IsSleeping() && asleepMaterial)
+        {
+            _wasAsleep = false;
+            _renderer.material = asleepMaterial;
+        }
+
+        //Set material to awake if rigidbody is awake
+        else if (!_wasAsleep && !_rigidbody.IsSleeping() && awakeMaterial)
+        {
+            _wasAsleep = true;
+            _renderer.material = awakeMaterial;
+        }
+    }
+}
