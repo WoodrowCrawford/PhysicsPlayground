c78ec08ee6277d24cdf26f2b21805dbc6712fdad
Woodrow Crawford <woodrowcrawford03@gmail.com>
2021-08-27 16:00:08 -0500

    Working on decorating car. Need to fix 2 more wheels

---
+++ Assets/Scripts/VehicleBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class VehicleBehavior : MonoBehaviour
+{
+
+    HingeJoint UpperLeftMotor;
+    HingeJoint UpperRightMotor;
+    HingeJoint LowerLeftMotor;
+    HingeJoint LowerRightMotor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        JointMotor leftMotor = UpperLeftMotor.motor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
