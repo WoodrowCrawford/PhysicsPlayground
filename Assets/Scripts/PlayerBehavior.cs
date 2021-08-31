ccac385a7d24473208251448063251d183f75358
Woodrow Crawford <woodrowcrawford03@gmail.com>
2021-08-31 15:52:03 -0500

    made player controls relative to the camera

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -7,27 +7,40 @@ public class PlayerBehavior : MonoBehaviour
     [SerializeField]
     public float speed = 5.0f;
     public float jumpForce = 10.0f;
+    public float airControl = 1.0f;
     public float gravityModifier = 1.0f;
+
+    public Camera playerCamera;
 
     private CharacterController _controller;
 
     private Vector3 _desiredGroundVelocity;
     private Vector3 _desiredAirVelocity;
-    private bool _isJumpDesired;
+    private bool _isJumpDesired = false;
+    public  bool _isGrounded = false;
 
 
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
     }
+
     private void Update()
     {
-        
-
         //Get movement input
         _desiredGroundVelocity.x = Input.GetAxis("Horizontal");
         _desiredGroundVelocity.y = 0.0f;
         _desiredGroundVelocity.z = Input.GetAxis("Vertical");
+
+        //Get camera normal
+        Vector3 cameraForward = playerCamera.transform.forward;
+
+        cameraForward.y = 0.0f;
+        cameraForward.Normalize();
+        //Get camera right
+        Vector3 cameraRight = playerCamera.transform.right;
+
+        _desiredGroundVelocity = (_desiredGroundVelocity.x * cameraRight + _desiredGroundVelocity.z * cameraForward);
 
         //Get jump input
         _isJumpDesired = Input.GetButtonDown("Jump");
@@ -36,19 +49,39 @@ public class PlayerBehavior : MonoBehaviour
         _desiredGroundVelocity.Normalize();
         _desiredGroundVelocity *= speed;
 
+    }
+
+    private void FixedUpdate()
+    {
+
+       
+        //Check for ground
+        _isGrounded = _controller.isGrounded;
+
         //Apply jump force
-        if (_isJumpDesired)
+        if (_isJumpDesired && _controller.isGrounded)
         {
             _desiredAirVelocity.y = jumpForce;
             _isJumpDesired = false;
         }
 
+        //stop on ground
+        if (_isGrounded && _desiredAirVelocity.y < 0.0f)
+        {
+            _desiredAirVelocity.y = -0.1f;
+        }
+
         //Apply gravity
-        _desiredAirVelocity += Physics.gravity * gravityModifier * Time.deltaTime;
+        _desiredAirVelocity += Physics.gravity * gravityModifier * Time.fixedDeltaTime;
+
         _desiredGroundVelocity += _desiredAirVelocity;
 
+        
+
         //Moves
-        _controller.Move((_desiredGroundVelocity + _desiredAirVelocity) * Time.deltaTime);
+        _controller.Move((_desiredGroundVelocity + _desiredAirVelocity) * Time.fixedDeltaTime);
+
+       
     }
    
 }
