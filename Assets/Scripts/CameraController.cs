5d2ad78dd2e8c125885b7b4628461f6ff868fd97
Woodrow Crawford <woodrowcrawford03@gmail.com>
2021-08-31 17:06:48 -0500

    Added player camera controls. need to fix some issues with it

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float distanceFromTarget = 30.0f;
+    public float sensitivity = 5.0f;
+    public bool invertY = false;
+
+    private void Update()
+    {
+        //Rotate the camera
+        if(Input.GetMouseButton(1))
+        {
+            Vector3 angles = transform.eulerAngles;
+            Vector2 rotation;
+            rotation.x = Input.GetAxis("Mouse Y") * (invertY ? 1.0f :  1.0f);
+            rotation.y = Input.GetAxis("Mouse X");
+
+            //Look up and down by rotating around the x-axis
+            angles.x = Mathf.Clamp(angles.x + rotation.x * sensitivity * Time.deltaTime, 0.0f, 70.0f);
+
+            //look left and right by rotating around the Y axis
+            angles.y += rotation.y * sensitivity * Time.deltaTime;
+
+            //set the angles
+            transform.eulerAngles = angles;
+
+        }
+
+
+        //Move the camera
+        transform.position = target.position + (distanceFromTarget * -transform.forward);
+        
+    }
+}
