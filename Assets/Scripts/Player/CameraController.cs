80ec69f4d9c2b99019072a3b780d62cebc37f290
Woodrow Crawford <woodrowcrawford03@gmail.com>
2021-09-09 13:04:09 -0500

    Created a projectile launcher

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float maxDistance = 10.0f;
+    public float sensitivity = 5.0f;
+    public bool invertY = false;
+    public float relaxSpeed = 20.0f;
+
+    private float currentDistance;
+
+    private void Start()
+    {
+        currentDistance = maxDistance;
+    }
+
+    private void Update()
+    {
+        //Rotate the camera
+        if (Input.GetMouseButton(1))
+        {
+            //Store current angles
+            Vector3 angles = transform.eulerAngles;
+            //Get input
+            Vector2 rotation;
+            rotation.x = Input.GetAxis("Mouse Y") * (invertY ? 1.0f : -1.0f);
+            rotation.y = Input.GetAxis("Mouse X");
+            //Look up and down by rotating around the X-axis
+            angles.x += rotation.x * sensitivity;
+            angles.x = Mathf.Clamp(angles.x, 0.0f, 90.0f);
+            //Look left and right by rotating around the Y-axis
+            angles.y += rotation.y * sensitivity;
+            //Set the updated angles
+            transform.eulerAngles = angles;
+        }
+
+        //Move the camera
+        RaycastHit hitInfo;
+        if (Physics.Raycast(target.position, -transform.forward, out hitInfo, maxDistance))
+        {
+            //Zoom the camera in if there is a collision
+            currentDistance = hitInfo.distance;
+        }
+        else
+        {
+            //Ease the camera out to the max distance if there is no collision
+            currentDistance = Mathf.MoveTowards(currentDistance, maxDistance, relaxSpeed * Time.deltaTime);
+        }
+        //Update the camera's position
+        transform.position = target.position + (-transform.forward * currentDistance);
+    }
+}
---
+++ Assets/Scripts/Player/ProjectileLauncherBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLauncherBehavior : MonoBehaviour
+{
+    public Transform target;
+    public Rigidbody projectile;
+
+    public float travelTime;
+
+    private Vector3 _displacement = new Vector3();
+    private Vector3 _acceleration = new Vector3();
+    private float _time = 0.0f;
+    private Vector3 _initialVelocity = new Vector3();
+    private Vector3 _finalVelocity = new Vector3();
+
+    public void LaunchProjectile()
+    {
+        _displacement = transform.position - target.position;
+        _acceleration = Physics.gravity;
+        _time = travelTime;
+        _initialVelocity = FindInitialVelocity(_displacement, _acceleration, _time);
+        _finalVelocity = FindFinalVelocity(_initialVelocity, _acceleration, _time);
+
+      Rigidbody projectileInstance = Instantiate(projectile, transform.position, transform.rotation);
+        projectileInstance.AddForce(_initialVelocity);
+       
+    }
+
+    private Vector3 FindFinalVelocity(Vector3 initialVelocity, Vector3 acceleration, float time)
+    {
+        //v = v0 + at
+        Vector3 finalVelocity = initialVelocity + acceleration * time;
+
+        return finalVelocity;
+
+    }
+
+    private Vector3 FindDisplacement(Vector3 initialVelocity, Vector3 acceleration, float time)
+    {
+        
+        Vector3 displacement = initialVelocity * time + (1 / 2) * acceleration * time * time;
+        return displacement;
+    }
+
+    private Vector3 FindInitialVelocity(Vector3 displacment, Vector3 acceleration, float time)
+    {
+        //deltaX = v0 * t + (1/2)*a*t^2
+        //deltaX - (1/2)*a*t^2 = v0*t
+        //deltaX/t - (1/2)*a*t = v0
+        //v0 = deltaX/t - (1/2)*a*t
+        Vector3 initialVelocity = displacment - (1 / 2) * acceleration * time;
+
+        return initialVelocity;
+    }
+
+   
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            LaunchProjectile();
+        }
+    }
+
+}
