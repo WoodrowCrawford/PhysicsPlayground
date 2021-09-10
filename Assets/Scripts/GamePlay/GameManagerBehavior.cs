15ddb63309bc4da486b77e491540b57f4c53c7d0
WoodrowCrawford <woodrowcrawford03@gmail.com>
2021-09-10 16:01:40 -0500

    Added a door mechanic

--- Assets/Scripts/GamePlay/GameManagerBehavior.cs
+++ Assets/Scripts/GamePlay/GameManagerBehavior.cs
@@ -6,17 +6,18 @@ using UnityEngine.UI;
 public class GameManagerBehavior : MonoBehaviour
 {
     private PlayerBehavior _player;
-
+    private Animator _animator;
 
 
     private void Awake()
     {
         //Gets the component of the player Behavior script
         _player = GetComponent<PlayerBehavior>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Start()
     {
-        
+        _animator.enabled = true;
     }
 }
